6726f193da2b8b84237816e106d8d8d10d35b7d6
Januar Elsan <januarelsan@gmail.com>
2019-10-15 16:21:18 +0700

    Upload Project

---
+++ index.js
@@ -0,0 +1,46 @@
+var io = require('socket.io')(process.env.PORT || 52300)
+
+//Custom class
+var Account = require('./Classes/Account.js');
+
+console.log('Server has started Bro!');
+
+var accounts = [];
+var sockets = [];
+
+io.on('connection', function(socket) {
+    console.log("Connection Made Bro!");
+
+    var account = new Account();
+    var thisAccountID = account.id;
+
+    accounts[thisAccountID] = account;
+    sockets[thisAccountID] = socket;
+
+    //tell the client that this is our id for the server    
+    socket.emit('register', {id: thisAccountID});
+    socket.emit('spawn', account); //Tell my self i have spawned
+    socket.broadcast.emit('spawn', account); //Tell other i have spawned
+
+    //Tell my self about everyone else in the game
+    for (var accountID in accounts) {
+        if(accountID != thisAccountID){
+            socket.emit('spawn', accounts[accountID]);
+        }
+    }
+
+    //Position data from client
+    socket.on('updatePosition', function(data){
+        account.position.x = data.position.x;
+        account.position.y = data.position.y;
+
+        socket.broadcast.emit('updatePosition', account);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('Someone has Disconnect from Server!');
+        delete accounts[thisAccountID];
+        delete sockets[thisAccountID];
+        socket.broadcast.emit('disconnected', account);
+    })
+});
